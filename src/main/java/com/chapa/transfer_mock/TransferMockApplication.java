015869fb2b36975abd82e074a0d0dc14405cbd5d
=biruksolomon <=biruksolomonmoges@gmail.com>
2025-12-23 19:19:37 +0300

    Initalizing

---
+++ src/main/java/com/chapa/transfer_mock/TransferMockApplication.java
@@ -0,0 +1,13 @@
+package com.chapa.transfer_mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chapa/transfer_mock/TransferMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chapa.transfer_mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
